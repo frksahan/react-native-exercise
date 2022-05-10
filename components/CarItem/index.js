70ea8f767de5861ce96831de9699058af4ca1f09
Ömer Faruk <sahan.faruk.omer@outlook.com>
2022-05-10 11:26:24 +0300

    react native

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, ImageBackground, } from 'react-native';
+import StyledButton from "../StyledButton";
 import styles from './styles';
 
 
@@ -16,6 +17,22 @@ const CarItem = (props) => {
                 <Text style={styles.textHeader} >Şahin S</Text>
                 <Text style={styles.text} >15.000'den başlayan fiyatlarla...</Text>
             </View>
+
+            <StyledButton
+                type="primary"
+                content={"Custom Order"}
+                onPress={() => {
+                    console.warn("To");
+                }}
+            />
+
+            <StyledButton
+                type="secondary"
+                content={"Existing Inventory"}
+                onPress={() => {
+                    console.warn("Existing Inventory was pressed");
+                }}
+            />
         </View>
     );
 };
